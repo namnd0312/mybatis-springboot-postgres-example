e7f45ac1169677125cbf9fd69a234bd7cd3680bb
namnd0312 <namnd0312@gmail.com>
2021-12-21 22:14:19 +0700

    init project

---
+++ src/main/java/com/namnd/springbootmybatisexample/SpringbootMybatisExampleApplication.java
@@ -0,0 +1,12 @@
+package com.namnd.springbootmybatisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootMybatisExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisExampleApplication.class, args);
+    }
+}
